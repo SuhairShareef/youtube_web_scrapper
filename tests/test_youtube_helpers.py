d28e52a1f5fba077c7c724f152f8cdd68fd2f1c0
suhairshareef <suhairshareef1999@gmail.com>
2023-05-16 21:30:41 +0300

    fix

--- tests/test_youtube_helpers.py
+++ tests/test_youtube_helpers.py
@@ -2,22 +2,23 @@ import json
 from unittest import TestCase
 
 from helpers import transpose_comments
+from utils.definitions import COMMENTS_MOCK_FILE_PATH
 
 
 class TestYouTubeHelpers(TestCase):
     def setUp(self) -> None:
-        with open("comments_examples.json") as json_mock_data_file:
+        with open(COMMENTS_MOCK_FILE_PATH) as json_mock_data_file:
             self.comment = json.load(json_mock_data_file)
 
     def test_transpose_comments(self):
         response = transpose_comments(self.comment)
 
         expected_response = {
-           "comment_id": "UgzOpYaC4oojL5mdg0V4AaABAg",
-           "text": "Is there a way to use the API Key to run the query?\nI would like to run this from an AWS Lambda "
-                   "Function so a key would work best",
-           "likes": 0,
-           "replies": 1,
+            "comment_id": "UgzOpYaC4oojL5mdg0V4AaABAg",
+            "text": "Is there a way to use the API Key to run the query?\nI would like to run this from an AWS Lambda "
+            "Function so a key would work best",
+            "likes": 0,
+            "replies": 1,
         }
 
         self.assertEqual(response, expected_response)
