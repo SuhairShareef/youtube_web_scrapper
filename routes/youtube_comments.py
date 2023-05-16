cb93235994c550e800bfb9dd96c1c6fa86c34f00
suhairsharif <suhair.s@cybord.ai>
2023-05-16 12:13:04 +0300

    Add youtube comments api

---
+++ routes/youtube_comments.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, HTTPException
+
+from googleapiclient.errors import HttpError
+from starlette import status
+
+import helpers
+from utils.definitions import ROOT_PATH, OK_RESP
+
+youtube_router = APIRouter(
+    prefix=ROOT_PATH + "/youtube",
+)
+
+
+@youtube_router.get(
+    "/comments",
+    status_code=200 | 400,
+    tags=["YouTube"],
+)
+async def get_comments_from_video(youtube_video_id) -> dict:
+    try:
+        comments = helpers.get_comments(youtube_video_id=youtube_video_id)
+        return {
+            "detail": OK_RESP,
+            "data": comments,
+        }
+
+    except Exception as exc:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=exc.args[0],
+        ) from exc
